6e1471e1f69855c7dd5068a51700331bb244133d
liang <liang@163.com>
2023-07-21 01:12:58 +0800

    使用brevitas量化模型并进行训练和保存

--- QuantizedModel.py
+++ QuantizedModel.py
@@ -15,7 +15,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 import brevitas.nn as qnn
-from concrete.ml.torch.compile import compile_brevitas_qat_model
+# from concrete.ml.torch.compile import compile_brevitas_qat_model
 
 class model(nn.Module):
     def __init__(self):
@@ -73,3 +73,4 @@ for epoch in range(10):
         if batch % 100 == 0:
             print('\tbatch: %d, loss: %.4f' % (batch, loss))
     print('epoch: %d， acc: %.2f%%, loss: %.4f' % (epoch, 100 * acc / sum, loss_sum / len(train_loader)))
+torch.save(net.state_dict(),'./model/quantized_model.pkl')

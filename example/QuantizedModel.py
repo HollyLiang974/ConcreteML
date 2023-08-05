575d239a5e2f8be8d76131789a7cb1f275597397
HollyLiang <lianghonglian521@163.com>
2023-08-05 18:57:48 +0800

    changge

--- example/QuantizedModel.py
+++ example/QuantizedModel.py
@@ -16,29 +16,34 @@ import torch.nn.functional as F
 import torchvision
 import brevitas.nn as qnn
 # from concrete.ml.torch.compile import compile_brevitas_qat_model
-
+import time
 class model(nn.Module):
     def __init__(self):
         super(model, self).__init__()
-        self.conv1 = qnn.QuantConv2d(1, 32, 3, weight_bit_width=8, bias=False)
-        self.relu1 = nn.ReLU()
+        self.quant_inp = qnn.QuantIdentity(bit_width=4)
+        self.conv1 = qnn.QuantConv2d(1, 32, 3, weight_bit_width=4, bias=False)
+        self.relu1 =  qnn.QuantReLU(bit_width=4)
         self.maxpool1 = nn.MaxPool2d(2, 2)
-        self.conv2 = qnn.QuantConv2d(32, 64, 3, weight_bit_width=8, bias=False)
-        self.relu2 = nn.ReLU()
+        self.conv2 = qnn.QuantConv2d(32, 64, 3, weight_bit_width=4, bias=False)
+        self.relu2 = qnn.QuantReLU(bit_width=4)
         self.maxpool2 = nn.MaxPool2d(2, 2)
         self.flatten = nn.Flatten()
-        self.linear1 = qnn.QuantLinear(64 * 5 * 5, 4096, weight_bit_width=8, bias=False)
-        self.relu3 = nn.ReLU()
-        self.linear2 = qnn.QuantLinear(4096, 10, weight_bit_width=8, bias=False)
+        self.linear1 = qnn.QuantLinear(64 * 5 * 5, 4096, weight_bit_width=4, bias=False)
+        self.relu3 = qnn.QuantReLU(bit_width=4)
+        self.linear2 = qnn.QuantLinear(4096, 10, weight_bit_width=4, bias=False)
 
     def forward(self, x):
+        x = self.quant_inp(x)
         x = self.conv1(x)
         x = self.relu1(x)
         x = self.maxpool1(x)
+        x = self.quant_inp(x)
         x = self.conv2(x)
         x = self.relu2(x)
         x = self.maxpool2(x)
+        x = self.quant_inp(x)
         x = self.flatten(x)
+        x = self.quant_inp(x)
         x = self.linear1(x)
         x = self.relu3(x)
         x = self.linear2(x)
@@ -54,6 +59,7 @@ optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 net = net.to(device)
 print('training on: ', device)
+start_time=time.time()
 for epoch in range(10):
     print('epoch:%d' % epoch)
     acc = 0.0
@@ -73,4 +79,6 @@ for epoch in range(10):
         if batch % 100 == 0:
             print('\tbatch: %d, loss: %.4f' % (batch, loss))
     print('epoch: %d， acc: %.2f%%, loss: %.4f' % (epoch, 100 * acc / sum, loss_sum / len(train_loader)))
+end_time=time.time()
+print("训练时长",end_time-start_time)
 torch.save(net.state_dict(), '../model/quantized_model.pkl')
